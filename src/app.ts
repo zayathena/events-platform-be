769f4ecef2b075a8010d558c8cf18bc653e76307
Clare Regan <claremaryregan@outlook.com>
2025-05-26 11:33:25 +0200

    changed port number

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import eventRoutes from './routes/eventRoutes';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
